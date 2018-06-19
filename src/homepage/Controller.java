e146122a2480f80e05f8c4ca3514fb5c4f45ae0e
Léo <leo.legron@gmail.com>
2018-06-19 22:19:44 +0200

    Initialisation

---
+++ src/homepage/Controller.java
@@ -0,0 +1,16 @@
+package homepage;
+
+import javafx.event.Event;
+
+public class Controller {
+
+    public void displayListLessons(Event event) {
+
+    }
+
+    public void displayListDiscussions(Event event) {
+    }
+
+    public void displayListQuizzes(Event event) {
+    }
+}
