80b843dd3afa0c49de5ff776b67ef7164e6e3f7c
Léo <leo.legron@gmail.com>
2018-07-05 17:14:24 +0200

    Allow pdf consultation

--- src/main/java/controller/menu/lesson/CourseListViewController.java
+++ src/main/java/controller/menu/lesson/CourseListViewController.java
@@ -13,17 +13,24 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.util.Callback;
 
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class CourseListViewController {
 
+    private final String FILE = "course.pdf";
+
+    private BorderPane insideBorderPane;
+    private ObservableList<Course> observableList = FXCollections.observableArrayList();
+
     @FXML
     private ListView<Course> courseListView;
 
-    private BorderPane insideBorderPane;
 
-    ObservableList<Course> observableList = FXCollections.observableArrayList();
 
     public CourseListViewController(BorderPane insideBorderPane) {
         this.insideBorderPane = insideBorderPane;
@@ -32,9 +39,6 @@ public class CourseListViewController {
         try
         {
             courseListView = fxmlLoader.load();
-            courseListView.setOnMouseClicked(event -> {
-                System.out.println(courseListView.getSelectionModel().getSelectedItem().getContent());
-            });
             this.insideBorderPane.setCenter(courseListView);
         }
         catch (IOException e)
@@ -43,11 +47,8 @@ public class CourseListViewController {
         }
     }
 
-    public ListView getCourseListView() {
-        return courseListView;
-    }
-
     public void setListView(String id) {
+        setListCellEvent();
         ESGIPocketProvider esgiPocketProvider = new ESGIPocketProvider(Authentification.getInstance().getToken());
         esgiPocketProvider.getCourses(id, new ApiListener<ArrayList<Course>>() {
             @Override
@@ -66,5 +67,55 @@ public class CourseListViewController {
 
     }
 
+    public void setListCellEvent(){
+        courseListView.setOnMouseClicked(event -> {
+            URL pdfUrl = null;
+            try {
+                pdfUrl = new URL(courseListView.getSelectionModel().getSelectedItem().getContent());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            read(pdfUrl);
+            open();
+        });
+    }
+
+    public void read(URL url){
+        try{
+            URLConnection connection = url.openConnection();
+            InputStream in = connection.getInputStream();
+            FileOutputStream fos = new FileOutputStream(new File(FILE));
+            byte[] buf = new byte[512];
+            while (true) {
+                int len = in.read(buf);
+                if (len == -1) {
+                    break;
+                }
+                fos.write(buf, 0, len);
+            }
+            in.close();
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void open(){
+        try{
+            File pdfFile = new File(FILE);
+            if (pdfFile.exists()){
+                if(Desktop.isDesktopSupported()){
+                    Desktop.getDesktop().open(pdfFile);
+                } else {
+                    System.out.println("Awt Desktop is not supported");
+                }
+            } else{
+                System.out.println("File doesn't exists");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
